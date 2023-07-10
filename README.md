73bd00fa3bf469b5df3dec1a78e83ce30523f149
statefb <tmgf.15.su@gmail.com>
2023-07-10 11:49:24 +0900

    first

--- README.md
+++ README.md
@@ -1,17 +1,48 @@
-## My Project
+# AWS EC2 Setup for Prototyping
 
-TODO: Fill this README out!
+## Overview
 
-Be sure to:
+This repository is designed to help you quickly start prototyping with AWS. It sets up an EC2 instance, configured to be accessed via Remote SSH such as [VSCode](https://code.visualstudio.com/docs/remote/ssh). The only requirement is an AWS account.
 
-* Change the title in this README
-* Edit your repository description on GitHub
+### What's included
 
-## Security
+This setup comes pre-installed with Python, Node.js, and Docker, which are commonly used for development in AWS.
 
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+## Getting started
 
-## License
+- Open [CloudShell](https://console.aws.amazon.com/cloudshell/home) in AWS.
+- Clone this repository.
 
-This library is licensed under the MIT-0 License. See the LICENSE file.
+```
+git clone https://github.com/statefb/aws-ec2-prototyping-environment
+```
 
+- Run the script. The first argument should be the access source IP's CIDR, the second argument is the stack name. **Substitute `XX.XX.XX.XX` to your environment accordingly.**
+
+```
+cd aws-ec2-prototyping-environment
+```
+
+```
+./bin.sh XX.XX.XX.XX/32 ProtoEnvStack
+```
+
+- Copy ssh key displayed on CloudShell to your clipboard, then save it locally as `prototype.pem`.
+- Change the permission of the pem file.
+
+```
+chmod 400 prototype.pem
+```
+
+- Login using SSH command. The IP address can be found at the bottom of output on CloudShell as `HostPublicIP`.
+
+```
+ssh -i prototype.pem ec2-user@XX.XX.XX.XX
+```
+
+- Follow the instructions provided by the respective IDE's documentation to connect the EC2 instance. For example, you can refer to VSCode's documentation [here](https://code.visualstudio.com/docs/remote/ssh).
+
+## Note
+
+The third and fourth argument to the ./bin.sh command can specify the instance size and volume size respectively.  
+By default, these are `t2.large` and `128GB`.
---
+++ bin.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+  echo "Insufficient arguments. Argument 1: Ipv4, Argument 2: Stack name"
+  exit 1
+fi
+
+Ipv4=$1
+StackName=$2
+InstanceType=$3
+VolumeSize=$4
+
+VPC_ID=$(aws ec2 describe-vpcs --filters "Name=isDefault,Values=true" --query "Vpcs[0].VpcId" --output text)
+echo "Default VPC ID is: $VPC_ID"
+
+PUBLIC_SUBNET_ID=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" "Name=map-public-ip-on-launch,Values=true" --query "Subnets[0].SubnetId" --output text)
+echo "Public Subnet ID is: $PUBLIC_SUBNET_ID"
+
+
+parameters="ParameterKey=Ipv4,ParameterValue=$Ipv4 ParameterKey=VpcId,ParameterValue=$VPC_ID ParameterKey=SubnetId,ParameterValue=$PUBLIC_SUBNET_ID"
+
+if [[ -n "$InstanceType" ]]; then
+  parameters+=" ParameterKey=InstanceType,ParameterValue=$InstanceType"
+fi
+
+if [[ -n "$VolumeSize" ]]; then
+  parameters+=" ParameterKey=VolumeSize,ParameterValue=$VolumeSize"
+fi
+
+stackId=$(aws cloudformation create-stack \
+  --stack-name $StackName \
+  --template-body file://template.json \
+  --parameters $parameters \
+  --capabilities CAPABILITY_IAM \
+  --query 'StackId' --output text)
+
+echo "Waiting for the stack creation to complete..."
+spin='-\|/'
+i=0
+while true; do
+    status=$(aws cloudformation describe-stacks --stack-name $StackName --query 'Stacks[0].StackStatus' --output text)
+    if [[ "$status" == "CREATE_COMPLETE" || "$status" == "UPDATE_COMPLETE" || "$status" == "DELETE_COMPLETE" ]]; then
+        break
+    fi
+    printf "\r${spin:i++%${#spin}:1}"
+done
+echo -e "\nDone!\n"
+
+outputs=$(aws cloudformation describe-stacks --stack-name $StackName --query 'Stacks[0].Outputs')
+echo "Copy ssh key from here: ==============================="
+echo "$outputs" | jq -r '.[] | select(.OutputKey == "GetSSHKeyCommand") | .OutputValue' | xargs -I {} sh -c {}
+echo -e "End of ssh key ===============================\n"
+
+hostPublicIp=$(echo "$outputs" | jq -r '.[] | select(.OutputKey == "HostPublicIp") | .OutputValue')
+echo "HostPublicIP: $hostPublicIp"
